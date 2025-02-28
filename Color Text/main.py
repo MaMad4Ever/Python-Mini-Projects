05317115ed6b803ca8e2b386c5e0e75719fc19a6
MaMad <113606119+MaMad4Ever@users.noreply.github.com>
2025-02-28 17:34:09 +0330

    Create main.py

---
+++ Color Text/main.py
@@ -0,0 +1,10 @@
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+print(Fore.RED + "Hello World")
+print(Fore.BLUE + "Hello World")
+print(Fore.GREEN + "Hello World")
+print(Fore.MAGENTA + "Hello World")
+print(Fore.YELLOW + "Hello World")
+print(Fore.CYAN + "Hello World")
